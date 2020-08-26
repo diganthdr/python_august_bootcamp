78f56a9766186a182179b60951dda14411ce3491
D R <diganth.d_r@nokia.com>
2020-08-26 10:33:29 +0530

    Database code, sqlite3

---
+++ project/db/dbhandle.py
@@ -0,0 +1,78 @@
+import sqlite3
+
+DB_NAME = 'covid_01.db'
+conn = sqlite3.connect(DB_NAME)
+c = conn.cursor()
+
+#one time activity after DB is created.
+#SQLite natively supports only the types TEXT, INTEGER, REAL, BLOB and NULL
+try:
+    cmd_create_table_state_cases = '''CREATE TABLE state_cases(day TEXT unique,state_name TEXT,active_cases INTEGER)'''
+    c.execute(cmd_create_table_state_cases)
+    conn.commit()
+except sqlite3.OperationalError as oe:
+    print("Table \'state_cases\' exists. Continuing with rest of the code...")
+
+try:
+    cmd_create_table_news = "CREATE TABLE news(day TEXT , url TEXT unique, headlines TEXT)"
+    c.execute(cmd_create_table_news)
+    conn.commit()
+except sqlite3.OperationalError as oe:
+    print("Table \'news\' exists. Continuing with rest of the code...")
+
+
+#Every run/day activity
+#cmd_insert_row = '''INSERT INTO state_cases VALUES ('2020-08-25','Karnataka', 3445)'''
+#cmd_insert_row = '''INSERT INTO state_cases VALUES ('2020-08-26','Karnataka', 3436)'''
+#c.execute(cmd_insert_row)
+#cmd_insert_row = '''INSERT INTO news VALUES('2020-08-26','test.com', "test headline")'''
+#c.execute(cmd_insert_row)
+
+
+def insert_row(table_name, row_content):
+    '''
+    Table name, input.
+    row_content is tuple.
+
+    form command from incoming row_content
+    '''
+    
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    cmd_insert_row = '''INSERT INTO %s VALUES %s''' %(table_name, row_content)
+    try:
+        c.execute(cmd_insert_row)
+        conn.commit()
+    except sqlite3.IntegrityError as ie:
+        print("Unique constraint, (with date) clashes. Skipping", str(ie))
+
+    conn.close()
+
+#c.execute(cmd_insert_row)
+#conn.commit()
+
+def get_rows(table_name, this_date,number_of_rows=0):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    from pdb import set_trace
+    set_trace()
+    #cmd_select_all_state_cases = "SELECT * FROM %s " %(table_name)
+    cmd_select_all_state_cases = "SELECT * FROM %s WHERE day = \'%s\'" %(table_name, this_date)
+    print()
+    rows = c.execute(cmd_select_all_state_cases)
+    print("Rows from state_cases")
+    for row in rows:
+        print(row) 
+
+#cmd_select_all_news = "SELECT * FROM news"
+#rows = c.execute(cmd_select_all_news)
+#print("Rows from news")
+#for row in rows:
+#    print(row) 
+
+#cmd_insert_row = '''INSERT INTO news VALUES('2020-08-26','test.com', "test headline")'''
+#insert_row('news', ('2020-08-25','test.com', "test headline"))
+#insert_row('news', ('2020-08-28','test2.com', "test headline"))
+#insert_row('news', ('2020-08-29','test2.com', "test headline"))
+#get_rows('news')
+get_rows('news', "2020-08-26", number_of_rows=0)
--- project/report/reports.py
+++ project/report/reports.py
@@ -1,10 +1,13 @@
+import time
+import datetime
+
 from inputdata.parse_input import get_input_data
 from news.news import get_news
 from subs.sub_data import get_subs_data
 from mail.mail import send_mail
 from report.generate_user_report import generate_user_report
 from report.generate_xls import generate_xlx_report
-
+from db.dbhandle import insert_row, get_rows
 
 def trigger():
     ''' aggregation of all modules here'''
@@ -33,10 +36,12 @@ def trigger():
     print("Processing data collected from NEWS module..")
     processed_news_text = '\n----- HEADLINES -----\n'
     for link, headline in news_map.items():
+        #import pdb;pdb.set_trace()
+        insert_row('news', (datetime.date.today().strftime('%Y-%m-%d'), link, headline))
         processed_news_text = processed_news_text + headline + TAB + link + NEWLINE
 
     print(processed_news_text)
-
+    get_rows('news', '2020-08-26')
     #6. Send mail with above data. 
     #   1. user specific data.
     #   2. XLS, attachment
@@ -56,5 +61,5 @@ def trigger():
         mail_text = mail_text + SIGNATURE + "\n"
         subject_line = "Python mini-project: COVID: Dashboard"
 
-        print("Sending mail to: ", user, "with:", mail_text)
-        send_mail(user, subject_line, mail_text, attachment=XLS_REPORT_FILENAME)
+        print("Sending mail to: ", user) #, "with:", mail_text)
+        #send_mail(user, subject_line, mail_text, attachment=XLS_REPORT_FILENAME)
